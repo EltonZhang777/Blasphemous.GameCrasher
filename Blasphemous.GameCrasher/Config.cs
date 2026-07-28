0999b6bb72eb767ea3ed88a9f91e6b4566e82d64
EltonZhang777 <280903158@qq.com>
2026-07-28 10:39:54 +0800

    feat(core): core game crashing logic
    - add config to detect specified mods to crash the game

---
+++ Blasphemous.GameCrasher/Config.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Blasphemous.GameCrasher;
+
+internal class Config
+{
+    /// <summary>
+    /// The list of mod IDs that, when detected, will crash the game
+    /// </summary>
+    public List<string> TargetMods { get; set; } = [];
+}
--- Blasphemous.GameCrasher/GameCrasher.cs
+++ Blasphemous.GameCrasher/GameCrasher.cs
@@ -1,13 +1,27 @@
 ﻿using Blasphemous.ModdingAPI;
+using Blasphemous.ModdingAPI.Helpers;
 
 namespace Blasphemous.GameCrasher;
 
+/// <inheritdoc />
 public class GameCrasher : BlasMod
 {
     internal GameCrasher() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
+    /// <inheritdoc />
     protected override void OnInitialize()
     {
-        // Perform initialization here
+        // Read target mods from config (auto-creates file on first run)
+        Config config = ConfigHandler.Load<Config>();
+
+        // Check if any target mod is loaded, crash game if yes
+        foreach (string targetId in config.TargetMods)
+        {
+            if (ModHelper.IsModLoadedById(targetId))
+            {
+                ModLog.Error($"Detected loaded mod `{targetId}`, crashing game!");
+                UnityEngine.Application.Quit();
+            }
+        }
     }
 }
